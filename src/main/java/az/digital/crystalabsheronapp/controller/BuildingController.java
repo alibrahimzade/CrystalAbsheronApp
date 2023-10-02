4f98b5ede0bdbb3de4b7146d7669fd61479d9c16
Ali <ibrahimzade.ali99@gmail.com>
2023-10-02 12:54:01 +0400

    buildings are updated

---
+++ src/main/java/az/digital/crystalabsheronapp/controller/BuildingController.java
@@ -0,0 +1,42 @@
+package az.digital.crystalabsheronapp.controller;
+
+import az.digital.crystalabsheronapp.dto.BuildingDto;
+import az.digital.crystalabsheronapp.service.BuildingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/building")
+public class BuildingController {
+
+    private final BuildingService buildingService;
+
+    @GetMapping("/getAll")
+    public ResponseEntity<?> getAllBuildings() {
+        return buildingService.getAllBuilding();
+    }
+
+    @GetMapping("/getById/{id}")
+    public ResponseEntity<?> getBuildingById(@PathVariable(name = "id") Long id) {
+        return buildingService.getBuildingById(id);
+    }
+
+    @PutMapping("/updateBuilding/{id}")
+    public ResponseEntity<?> updateBuilding(@RequestBody BuildingDto buildingDto, @PathVariable(name = "id") Long buildingId) {
+        return ResponseEntity.ok(buildingService.updateBuilder(buildingId, buildingDto));
+
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteById(Long id) {
+        return buildingService.deleteBuilding(id);
+
+    }
+
+    @PostMapping("/createdBuilding")
+    public void saveBuilding(@RequestBody BuildingDto buildingDto){
+        buildingService.createBuilding(buildingDto);
+    }
+}
--- src/main/java/az/digital/crystalabsheronapp/service/BuildingService.java
+++ src/main/java/az/digital/crystalabsheronapp/service/BuildingService.java
@@ -4,14 +4,17 @@ import az.digital.crystalabsheronapp.dao.entity.Building;
 import az.digital.crystalabsheronapp.dao.repository.BuildingRepository;
 import az.digital.crystalabsheronapp.dto.BuildingDto;
 import az.digital.crystalabsheronapp.mapper.BuildingMapper;
-import az.digital.crystalabsheronapp.request.BuildingRequest;
-import az.digital.crystalabsheronapp.response.BuildingResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+
+import static org.hibernate.engine.spi.Status.DELETED;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
 
 @Service
 @RequiredArgsConstructor
@@ -26,10 +29,37 @@ public class BuildingService {
 
     }
 
-    public ResponseEntity<BuildingResponse> createBuilding(BuildingRequest buildingRequest){
+    public ResponseEntity<?> getBuildingById(Long id) {
+        Building building = buildingRepository.findById(id).orElseGet(null);
+        if (Objects.nonNull(building)) {
+            return ResponseEntity.ok(BuildingMapper.BUILDING_MAPPER.buildEntityToDto(building));
+        }
+        return ResponseEntity.status(NOT_FOUND).body("THIS BUILDING IS NOT EXIST");
+    }
 
-        buildingRepository.save(BuildingMapper.)
+    public void createBuilding(BuildingDto buildingDto) {
+
+        buildingRepository.save(BuildingMapper.BUILDING_MAPPER.buildDtoToEntity(buildingDto));
+    }
+
+    public ResponseEntity<?> updateBuilder(Long buildingId, BuildingDto buildingDto) {
+        Building building = buildingRepository.findById(buildingId).orElseGet(null);
+        if (Objects.nonNull(building)) {
+            Building update = BuildingMapper.BUILDING_MAPPER.buildDtoToEntity(buildingDto);
+            return ResponseEntity.ok(buildingRepository.save(update));
+        }
+
+        return ResponseEntity.status(NOT_FOUND).body("BUILDING NOT FOUND");
 
     }
 
+    public ResponseEntity<?> deleteBuilding(Long id) {
+        Building building = buildingRepository.findById(id).orElseGet(null);
+        if (Objects.nonNull(building)) {
+            buildingRepository.deleteById(id);
+            return ResponseEntity.ok(DELETED);
+        }
+        return ResponseEntity.status(NOT_FOUND).body("THIS BUILDING IS NOT EXIST");
+
+    }
 }
