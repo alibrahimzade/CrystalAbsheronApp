be1659fb5a636cc96964ff9b7f5117aae30b168c
Ali <ibrahimzade.ali99@gmail.com>
2023-10-09 12:27:59 +0400

    Keks

--- src/main/java/az/digital/crystalabsheronapp/config/SecurityConfig.java
+++ src/main/java/az/digital/crystalabsheronapp/config/SecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-@EnableWebSocketSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final JwtFilter jwtFilter;
