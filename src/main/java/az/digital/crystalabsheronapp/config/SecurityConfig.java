a94f0f320a185233c60484c0a97e67089f2ea67e
shamerdanov <shamil332211@list.ru>
2023-10-09 14:54:01 +0400

    changes of email sender by shamil

--- src/main/java/az/digital/crystalabsheronapp/config/SecurityConfig.java
+++ src/main/java/az/digital/crystalabsheronapp/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
                 .requestMatchers("/building/**").permitAll()
                 .requestMatchers("/user/**").permitAll()
                 .requestMatchers("/residence/**").permitAll()
+                .requestMatchers("/mail/**").permitAll()
                 .requestMatchers("/api/information/**").permitAll()
                 .requestMatchers("/chat/**").permitAll()
                 .anyRequest()
