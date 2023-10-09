5fea3b5f2ff0060a58c8b5103350ef319e0af325
Ali <ibrahimzade.ali99@gmail.com>
2023-10-09 14:54:52 +0400

    WebSocket changes from ali

---
+++ src/main/java/az/digital/crystalabsheronapp/chat/ChatMessage.java
@@ -0,0 +1,14 @@
+package az.digital.crystalabsheronapp.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private MessageType type;
+}
