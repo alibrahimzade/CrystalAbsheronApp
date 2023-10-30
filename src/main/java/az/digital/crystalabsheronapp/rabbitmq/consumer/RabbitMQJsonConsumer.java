fcfe82ce2cf3a591c7d71a9723bdb2fe7d2a47d9
shamerdanov <shamil332211@list.ru>
2023-10-30 12:49:10 +0400

    security config class updated

--- src/main/java/az/digital/crystalabsheronapp/rabbitmq/consumer/RabbitMQJsonConsumer.java
+++ src/main/java/az/digital/crystalabsheronapp/rabbitmq/consumer/RabbitMQJsonConsumer.java
@@ -1,20 +1,20 @@
-package az.digital.crystalabsheronapp.rabbitmq.consumer;
-
-import az.digital.crystalabsheronapp.rabbitmq.dto.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Service;
-
-@Service
-public class RabbitMQJsonConsumer {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQJsonConsumer.class);
-
-    @RabbitListener(queues = {"${rabbitmq.queue.json.name}"})
-    public void consumeJsonMessage(User user){
-        LOGGER.info(String.format("Received JSON message -> %s", user));
-
-    }
-
-}
+//package az.digital.crystalabsheronapp.rabbitmq.consumer;
+//
+//import az.digital.crystalabsheronapp.rabbitmq.dto.User;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.amqp.rabbit.annotation.RabbitListener;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class RabbitMQJsonConsumer {
+//
+//    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQJsonConsumer.class);
+//
+//    @RabbitListener(queues = {"${rabbitmq.queue.json.name}"})
+//    public void consumeJsonMessage(User user){
+//        LOGGER.info(String.format("Received JSON message -> %s", user));
+//
+//    }
+//
+//}
